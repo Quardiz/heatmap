998fb8101dab93dcb99227dd97dc7a3818ada03d
Quardiz <quardiz@protonmail.com>
2020-09-03 14:00:07 +0200

    Updates

---
+++ precache-manifest.b969d8de467eed2139301da9bc1eafe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3109ed4f6f731cc4559c72e502fe9653",
+    "url": "/heatmap/index.html"
+  },
+  {
+    "revision": "206c3f14eb1dc3184b4f",
+    "url": "/heatmap/static/css/main.9cf82261.chunk.css"
+  },
+  {
+    "revision": "cf399152f64898e861d1",
+    "url": "/heatmap/static/js/2.857b9edd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heatmap/static/js/2.857b9edd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206c3f14eb1dc3184b4f",
+    "url": "/heatmap/static/js/main.1b270ac2.chunk.js"
+  },
+  {
+    "revision": "868ec971d7990c0f1309",
+    "url": "/heatmap/static/js/runtime-main.1dbb8245.js"
+  }
+]);
